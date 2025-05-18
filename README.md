497df912145554a7df26e05f764fe0512418ac91
Muhammad Aulia Jabbar <jabbar.aulia@gmail.com>
2025-05-18 10:26:26 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+ini merupakan repository dalam workshop OR14 Neo Telemetri
+
+Nama = Muhammad Aulia Jabbar
+Divisi = Programming
+Sub Divisi = Machine Learning
---
+++ main.py
@@ -0,0 +1,5 @@
+name = "muhammad aulia jabbar"
+division = "programming"
+sub_divisi = "machine learning"
+
+print(f"nama saya {name}, \ndivisi {division}, \nsub-divisi {sub_divisi}.")
